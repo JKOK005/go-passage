dee2ffa348bade9ed544a5db80ebd9062a94407c
johan.kok <johan.kok@grabtaxi.com>
2020-01-10 00:41:17 +0800

    Documentation on compiling protobuf messages in Master
    Blacklisted generated protoc code dir in .gitignore
    
    Completed implementation of gRPC callbacks for hello world and heart beating
    
    TODO:
    - Test implementation

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Passage delivery framework
 
 ## Description
 Passage is designed to facilitate efficient message passing between multiple applications deployed in a cluster
+
+## PRotobuf
+Compile and generate Protobuf libraries for master
+```proto
+protoc -I api/protobuf-spec/proto/master --go_out=plugins=grpc:api/protobuf-spec/bin/master/ api/protobuf-spec/proto/master/*.proto
+```
--- pkg/passage-master/grpcHandlers.go
+++ pkg/passage-master/grpcHandlers.go
@@ -1,10 +1,26 @@
 package passage_master
 
 import (
+	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
+	"context"
+	pb "go-passage/api/protobuf-spec/bin/master"
 )
+
+type grpcClient struct {}
+
+func (g *grpcClient) HelloWorld(ctx context.Context, empty *empty.Empty) (*pb.HelloWorldResponse, error) {
+	resp := helloWorldHandler()
+	return &pb.HelloWorldResponse{Resp:resp}, nil
+}
+
+func (g *grpcClient) Heartbeat(ctx context.Context, empty *empty.Empty) (*pb.HeartbeatResponse, error) {
+	resp := heartbeatHandler()
+	return &pb.HeartbeatResponse{Resp:resp}, nil
+}
 
 func DispatchGrpcApiHandler() *grpc.Server {
 	grpcServer := grpc.NewServer()
+	pb.RegisterMasterServicesServer(grpcServer, &grpcClient{})
 	return grpcServer
 }
