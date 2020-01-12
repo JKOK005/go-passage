3e6309eec92fcb19b912c81bef24c34c31af649c
johan.kok <johan.kok@grabtaxi.com>
2020-01-13 00:19:02 +0800

    Expanding functionalities for grpcHandlers - Added add / remove server / application skeletal code
    Added new request / response proto messages
    
    TODO:
    - Complete other skeletal code body
    - Complete implementation of grpc Features

--- pkg/passage-master/grpcHandlers.go
+++ pkg/passage-master/grpcHandlers.go
@@ -23,10 +23,59 @@ func (g *grpcClient) Heartbeat(ctx context.Context, empty *empty.Empty) (*pb.Hea
 	Basic CRUD operations for Servers to register themselves, as well as the applications that are docked to them
 */
 
-func (g *grpcClient) AddPassageServer() {}
-func (g *grpcClient) RemovePassageServer() {}
-func (g *grpcClient) AddApplication() {}
-func (g *grpcClient) RemoveApplication() {}
+func (g *grpcClient) AddServer(ctx context.Context, request *pb.AddServerRequest) (*pb.AddServerResponse, error) {
+	/*
+		Registers a new Passage server. There can only be one master but multiple servers.
+
+		Params
+			- request: 	Add server request
+		Returns
+			- pb.AddServerResponse: proto add server response
+			- error: Error when adding server
+	*/
+	return nil,nil
+}
+
+func (g *grpcClient) RemoveServer(ctx context.Context, request *pb.RemoveServerRequest) (*pb.RemoveServerResponse, error) {
+	/*
+		Removes an existing Passage server
+
+		Params
+			- request: 	Remove server request
+		Returns
+			- pb.RemoveServerResponse: proto remove server response
+			- error: Error when adding server
+	*/
+	return nil,nil
+}
+
+func (g *grpcClient) AddApplication(ctx context.Context, request *pb.AddAppRequest) (*pb.AddAppResponse, error) {
+	/*
+		Adds an existing application registered already to a server. Each application is identified by its name.
+		There may be multiple applications sharing the same name but only one (serverID, appName) pair.
+
+		Params
+			- request: 	Add application request
+		Returns
+			- pb.AddAppResponse: proto add application response
+			- error: Error when adding server
+	*/
+	return nil,nil
+}
+
+func (g *grpcClient) RemoveApplication(ctx context.Context, request *pb.RemoveAppRequest) (*pb.RemoveAppResponse, error) {
+	/*
+		Removes an application registered already to a server, identified by unique (serverID, appName) pair
+
+		Params
+			- request: 	Remove application request
+		Returns
+			- pb.AddAppResponse: proto add application response
+			- error: Error when adding server
+	*/
+	return nil,nil
+}
+
 func (g *grpcClient) CheckServeIsAlive() {}
 func (g *grpcClient) GetAppAddress() {}
 
