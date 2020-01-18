ad73cf8a95f284ef727a130f1ad7ecf67ad5ec83
johan.kok <johan.kok@grabtaxi.com>
2020-01-18 15:36:22 +0800

    Refractored CRUD features for mysql.go - Added GetSErver / GetApp features - Renamed GetApp -> GetApps for getting the server addresses given an app name

--- pkg/passage-master/storage/mysql.go
+++ pkg/passage-master/storage/mysql.go
@@ -1,16 +1,27 @@
 package storage
 
-type MysqlClient struct {}
+import (
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type MysqlClient struct {
+	user string
+	password string
+	schema string
+}
+
 type ServerClient struct {
 	exposeUrl string
 	exposePort uint32
 }
 
-func (m *MysqlClient) AddServer(serverUrl string, serverPort uint32) (uint32, error) {return -1, nil}
+func (m *MysqlClient) AddServer(serverUrl string, serverPort uint32) error {return nil}
+func (m *MysqlClient) GetServer(serverUrl string, serverPort uint32) (uint32, error) {return -1, nil}
 func (m *MysqlClient) RemoveServer(serverID uint32) error {return nil}
-func (m *MysqlClient) AddApp(serverID uint32, appName string) (uint32, error) {return -1, nil}
+func (m *MysqlClient) AddApp(serverID uint32, appName string) error {return nil}
+func (m *MysqlClient) GetApp(serverID uint32, appName string) (uint32, error) {return -1, nil}
+func (m *MysqlClient) GetApps(appName string) ([]ServerClient, error) {return nil, nil}
 func (m *MysqlClient) RemoveApp(appID uint32) error {return nil}
-func (m *MysqlClient) GetAppLoc(appName string) ([]ServerClient, error) {return nil, nil}
 
 func CreateClient() (*MysqlClient, error) {
 	/*
