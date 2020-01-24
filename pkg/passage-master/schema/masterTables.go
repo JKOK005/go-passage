3e7c20f7ea355f35cc18139d560244e73d81f454
johan.kok <johan.kok@grabtaxi.com>
2020-01-24 16:44:27 +0800

    Changed model type of DeployedServerUid from uint -> int - For compatibility with other functions using the model

--- pkg/passage-master/schema/masterTables.go
+++ pkg/passage-master/schema/masterTables.go
@@ -23,7 +23,7 @@ func (s ServerModel) ToString() string {
 type AppModel struct {
 	gorm.Model
 	Name string `gorm:"type:varchar(20); NOT NULL; unique_index:indx"`
-	DeployedServerUid uint `gorm:"NOT NULL; unique_index:indx"`
+	DeployedServerUid int `gorm:"NOT NULL; unique_index:indx"`
 }
 
 func (a AppModel) TableName() string {
