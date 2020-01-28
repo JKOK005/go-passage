8af9651dd912df10d055f54bfa49c9e2168f1765
johan.kok <johan.kok@grabtaxi.com>
2020-01-29 00:24:17 +0800

    Removed unnecessary comments in passage master - Comments were unnecessary

--- pkg/passage-master/server/grpcHandlers.go
+++ pkg/passage-master/server/grpcHandlers.go
@@ -30,12 +30,6 @@ func (g grpcClient) Heartbeat(ctx context.Context, empty *empty.Empty) (*pb.Hear
 func (g grpcClient) AddServer(ctx context.Context, request *pb.AddServerRequest) (*pb.AddServerResponse, error) {
 	/*
 		Registers a new Passage server. There can only be one master but multiple servers.
-
-		Params
-			- request: 	Add server request
-		Returns
-			- pb.AddServerResponse: proto add server response
-			- error: Error when adding server
 	*/
 	err := g.Handler.RegisterServer(request.ExposedUrl, int(request.ExposedPort)); if err != nil {return nil, err}
 
@@ -52,12 +46,6 @@ func (g grpcClient) AddServer(ctx context.Context, request *pb.AddServerRequest)
 func (g grpcClient) RemoveServer(ctx context.Context, request *pb.RemoveServerRequest) (*pb.RemoveServerResponse, error) {
 	/*
 		Removes an existing Passage server
-
-		Params
-			- request: 	Remove server request
-		Returns
-			- pb.RemoveServerResponse: proto remove server response
-			- error: Error when adding server
 	*/
 	err := g.Handler.DeleteServer(int(request.ServerID)); if err != nil {return nil, err}
 	return &pb.RemoveServerResponse{Resp: true},nil
@@ -67,12 +55,6 @@ func (g grpcClient) AddApplication(ctx context.Context, request *pb.AddAppReques
 	/*
 		Adds an existing application registered already to a server. Each application is identified by its name.
 		There may be multiple applications sharing the same name but only one (serverID, appName) pair.
-
-		Params
-			- request: 	Add application request
-		Returns
-			- pb.AddAppResponse: proto add application response
-			- error: Error when adding server
 	*/
 	err := g.Handler.RegisterApp(int(request.ServerID), request.AppName); if err != nil {return nil, err}
 	return &pb.AddAppResponse{Resp: true}, nil
@@ -81,12 +63,6 @@ func (g grpcClient) AddApplication(ctx context.Context, request *pb.AddAppReques
 func (g grpcClient) RemoveApplication(ctx context.Context, request *pb.RemoveAppRequest) (*pb.RemoveAppResponse, error) {
 	/*
 		Removes an application registered already to a server, identified by unique (serverID, appName) pair
-
-		Params
-			- request: 	Remove application request
-		Returns
-			- pb.AddAppResponse: proto add application response
-			- error: Error when adding server
 	*/
 	err := g.Handler.DeleteApp(int(request.ServerID), request.AppName); if err != nil {return nil, err}
 	return &pb.RemoveAppResponse{Resp: true}, nil
@@ -109,12 +85,6 @@ func (g grpcClient) GetAppAddress(ctx context.Context, request *pb.GetAppRequest
 	/*
 		Returns address of the application present within each server, if the application does exist.
 		Else, we return an empty response
-
-		Params
-			- request: Get Application address request
-		Returns
-			- pb.GetAppResponse: If application exists, return the message type GetAppResponse, else return nothing
-			- error: Error when attempting to retrieve application
 	*/
 	hostServers, err := g.Handler.SearchApp(request.AppName); if err != nil {
 		return nil, err
